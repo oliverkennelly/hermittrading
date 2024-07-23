4c07b2c8036b695a603bff5061f3cadece18004b
Oliver Kennelly <victoriakennelly@gmail.com>
2024-07-23 11:32:54 -0500

    routing and users set up

---
+++ hermitapi/tradeapi/views/__init__.py
@@ -0,0 +1,6 @@
+from materialprices import MaterialPriceViewSet
+from materials import MaterialsViewSet
+from playerinventories import PlayerInventoryViewSet
+from playerstatuses import PlayerStatusViewSet
+from towns import TownViewSet
+from users import UserViewSet
