75535c5088e4cb1285789e0a7818b544e685dc34
Oliver Kennelly <victoriakennelly@gmail.com>
2024-07-23 12:11:16 -0500

    database seeding

--- hermitapi/seed_database.sh
+++ hermitapi/seed_database.sh
@@ -7,4 +7,8 @@ python3 manage.py makemigrations tradeapi
 python3 manage.py migrate tradeapi
 python3 manage.py loaddata users
 python3 manage.py loaddata tokens
-
+python3 manage.py loaddata materials
+python3 manage.py loaddata towns
+python3 manage.py loaddata player_statuses
+python3 manage.py loaddata player_inventories
+python3 manage.py loaddata material_prices
