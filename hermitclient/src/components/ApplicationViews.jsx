d3fb1d74de2c875e4aae8f354c37433b95b0210a
Oliver Kennelly <victoriakennelly@gmail.com>
2024-07-28 21:42:45 -0500

    routing and modules set up

--- hermitclient/src/components/ApplicationViews.jsx
+++ hermitclient/src/components/ApplicationViews.jsx
@@ -1,14 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Authorized } from "./Authorized"
 import { Login } from "../pages/Login.jsx"
-import Home from "../pages/Home"
+import Map from "../pages/Map"
 import { Register } from '../pages/Register.jsx'
-import { Games } from "./Games.jsx"
-import { GameForm } from "./GameForm.jsx"
-import { ViewGame } from "./ViewGame.jsx"
-import { ReviewForm } from "./ReviewForm.jsx"
-import { EditGame } from "./EditGame.jsx"
-import { UploadPhotoForm } from "./UploadPhotoForm.jsx"
+import { Instructions } from "./Instructions.jsx"
+import { Shop } from "./Shop.jsx"
+import { EndResult } from "./EndResult.jsx"
 
 
 export const ApplicationViews = () => {
@@ -20,17 +17,12 @@ export const ApplicationViews = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route element={<Authorized />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/games" element={<Games authToken={authToken}/>} />
-                <Route path="/create" element={<GameForm authToken={authToken}/>}/>
-                <Route path="/view">
-                    <Route path=":gameId" element={<ViewGame authToken={authToken}/>}/>
+                <Route path="/" element={<Map />} />
+                <Route path="/instructions" element={<Instructions authToken={authToken}/>} />
+                <Route path="/shop">
+                    <Route path=":cityId" element={<Shop authToken={authToken}/>}/>
                 </Route>
-                <Route path="/view/:gameId/review" element={<ReviewForm authToken={authToken} />} />
-                <Route path="/view/:gameId/upload" element={<UploadPhotoForm authToken={authToken} />} />
-                <Route path="/edit">
-                    <Route path=":gameId" element={<EditGame authToken={authToken}/>}/>
-                </Route>
+                <Route path="/end" element={<EndResult authToken={authToken}/>} />
             </Route>
         </Routes>
     </BrowserRouter>
